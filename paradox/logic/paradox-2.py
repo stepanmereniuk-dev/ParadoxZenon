fc87613c15716d4955c7d2ba1a3487da26f0f618
stepanmereniuk-dev <stepan.mereniuk@laplateforme.io>
2026-09-01 13:20:45 +0200

    init commit

---
+++ paradox/logic/paradox-2.py
@@ -0,0 +1,15 @@
+import time
+
+if __name__ == "__main__":
+    s = 0
+    d = 0.5
+    f = 1
+    while s <= 1:
+        
+        s = s + d
+        d = d/2
+        time.sleep(1)
+        print(s)
+        pass
+
+    print("cc") 
